14ab92a1d42c39cfeb3f61431b18f9c70d518031
Anubhav Bhardwaj <37350430+anii234@users.noreply.github.com>
2024-08-27 20:29:52 +0530

    Update demo.tf

--- demo.tf
+++ demo.tf
@@ -1,8 +1,6 @@
 module "ec2-instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "5.6.1"
-  for_each = var.machine
-  name = each.value
   instance_type = "t2.micro"
   ami="ami-01b799c439fd5516a"
 }
